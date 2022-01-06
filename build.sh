6c60b097fc5a421be1ddf187c10c2e2be961a7bf
Andrews Arokiam <andrews.arokiam@ideas2it.com>
2022-01-06 19:13:49 +0530

    Test
    Signed-off-by: Andrews Arokiam <andrews.arokiam@ideas2it.com>

--- build.sh
+++ build.sh
@@ -3,11 +3,12 @@
 git config --local user.email "github-actions[bot]@users.noreply.github.com" 
 git config --local user.name "github-actions[bot]"
 currentVersion=$(cat version.txt)
+ls -al
+echo "Fetching latest gh-pages"
+git fetch origin gh-pages
 mike deploy $currentVersion
 ls -al
-echo "Checking out gh-pages"
-git checkout gh-pages
-mv $currentVersion "${currentVersion}-tmp"
-git clone -b gh-pages https://github.com/kserve/website.git
-ls -al
-mv "${currentVersion}-tmp" $currentVersion
+# mv $currentVersion "${currentVersion}-tmp"
+# git clone -b gh-pages https://github.com/kserve/website.git
+# ls -al
+# mv "${currentVersion}-tmp" $currentVersion
