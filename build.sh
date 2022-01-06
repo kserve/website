7297b312d37d0086a908197b0fdebf0bde9a3daa
Andrews Arokiam <andrews.arokiam@ideas2it.com>
2022-01-06 18:37:07 +0530

    build script changes
    Signed-off-by: Andrews Arokiam <andrews.arokiam@ideas2it.com>

--- build.sh
+++ build.sh
@@ -2,6 +2,12 @@
 
 git config --local user.email "github-actions[bot]@users.noreply.github.com" 
 git config --local user.name "github-actions[bot]"
-mike deploy $(cat version.txt)
-git checkout gh-pages
+currentVersion=$(cat version.txt)
+mike deploy $currentVersion
 ls -al
+echo "Checking out gh-pages"
+git checkout gh-pages
+mv $currentVersion "${currentVersion}-tmp"
+git clone -b gh-pages https://github.com/kserve/website.git
+ls -al
+mv "${currentVersion}-tmp" $currentVersion
