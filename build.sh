d367230d5ea9e561e5b7d438e82955c3955db425
Andrews Arokiam <andrews.arokiam@ideas2it.com>
2022-01-06 18:01:03 +0530

    Added debug logs
    Signed-off-by: Andrews Arokiam <andrews.arokiam@ideas2it.com>

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ git config --local user.email "github-actions[bot]@users.noreply.github.com"
 git config --local user.name "github-actions[bot]"
 mike deploy $(cat version.txt)
 git checkout gh-pages
+ls -al
