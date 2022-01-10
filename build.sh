e2ea743bd5245769d72b018e12074cf6264ccbe3
Andrews Arokiam <andrews.arokiam@ideas2it.com>
2022-01-10 12:23:55 +0530

    sDebugging
    Signed-off-by: Andrews Arokiam <andrews.arokiam@ideas2it.com>

--- build.sh
+++ build.sh
@@ -6,13 +6,20 @@ currentVersion=$(cat version.txt)
 ls -al
 echo "Fetching latest gh-pages"
 git fetch origin gh-pages --depth=1
-mike deploy $currentVersion
 
-echo "Checking out gh pages"
+git remote show
+
+tmp=$(date +%s)
+tmp_branch="gh-page-${tmp}"
+
+mike deploy $currentVersion -b $tmp_branch
+
+mv $currentVersion "/tmp/${currentVersion}"
+
 git checkout gh-pages
 
 ls -al
-# mv $currentVersion "${currentVersion}-tmp"
+# 
 # git clone -b gh-pages https://github.com/kserve/website.git
 # ls -al
 # mv "${currentVersion}-tmp" $currentVersion
